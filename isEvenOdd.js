fd244152c762b412ff65a53a262b29ca3584538a
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-07 02:43:12 +0600

    odd and even number check done

---
+++ isEvenOdd.js
@@ -0,0 +1,31 @@
+/* function isEven(number) {
+    const remainder = number % 2;
+    if (remainder == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const myNumberIsEven = isEven(303);
+console.log(myNumberIsEven);
+const herNumberIsEven = isEven(1280);
+console.log(herNumberIsEven); */
+
+// problem 05: Check Two Numbers As Input. If Number is Even Then Print True And Number is Odd Then Print False. Input Number (98 & 117)
+function checkOddEven(number) {
+    const remainder = number % 2;
+    if (remainder == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const razibNumber = 98;
+const razibInputNumber = checkOddEven(razibNumber);
+console.log('Razib Input Number:', razibInputNumber);
+const mamunNumber = 117;
+const mamunInputNumber = checkOddEven(mamunNumber);
+console.log('Mamun Input Number:', mamunInputNumber);
